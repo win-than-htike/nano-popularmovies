d474ff1514b4e44a21f3bed6eeb5c94e5e06c26c
whiteteenboy@gmail.com <ye-lin-aung>
2016-03-03 10:04:21 +0630

    remove keys

--- app/src/main/java/com/plan/yelinaung/popularmovies/utils/Constants.java
+++ app/src/main/java/com/plan/yelinaung/popularmovies/utils/Constants.java
@@ -6,10 +6,10 @@ package com.plan.yelinaung.popularmovies.utils;
 public class Constants {
   public static final String BASE_URL = "http://api.themoviedb.org/3/";
   public static final String BASE_IMAGE_URL = "http://image.tmdb.org/t/p/w185//";
-  //public static final String MOVIE_DB_API = "cae383be2635217d38b151b9c844c246";
+  public static final String MOVIE_DB_API = null;
   public static final String PARCEL_DETAIL_NAME = "detail_parcel";
   public static final String PARCEL_MOVIE_NAME = "movie_parcel";
-  public static final String YOUTUBE_APIKEY = "AIzaSyAGtEgsq6y1m6Hwtu6Bh3EfBckNGUKYSD4";
+  public static final String YOUTUBE_APIKEY = null;
 
 
 
